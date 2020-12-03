fc3165e5c49600d0215cc108270c68c071b89f3d
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-12-03 15:12:22 -0800

    updates backend database

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 // export const API_ROOT = "http://localhost:3000";
 // export const API_WS_ROOT = "ws://localhost:3000/cable";
-export const API_ROOT = "https://gentle-castle-53687.herokuapp.com";
-export const API_WS_ROOT = "wss://gentle-castle-53687.herokuapp.com/cable";
+export const API_ROOT = "https://intense-anchorage-02448.herokuapp.com";
+export const API_WS_ROOT = "wss://intense-anchorage-02448.herokuapp.com/cable";
 export const HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json",
